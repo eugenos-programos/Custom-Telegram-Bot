c1542a1bf2b9ef071ae74e75ea8a6f24d41c2ce1
eugenos-programos <elena.samchuk.73@mail.ru>
2023-09-18 20:01:21 +0300

    add FRED model and UserDialog classes

---
+++ src/FREDModel.py
@@ -0,0 +1,30 @@
+from typing import Any
+from peft import PeftModel
+import torch
+import transformers
+
+
+class FREDModel:
+    def __init__(self, path: str, device: torch.device) -> None:
+        model = transformers.T5ForConditionalGeneration.from_pretrained(path, load_in_8bit=True, device_map="auto")
+        self._tokenizer = transformers.GPT2Tokenizer.from_pretrained(path)
+        self._tokenizer.add_special_tokens({'bos_token': '<s>', 'eos_token': '</s>', 'pad_token': '<pad>'})
+        self._model = PeftModel.from_pretrained(model, path, device_map='auto', torch_dtype=torch.float16)
+        self._model.eval()
+        self.device = device
+        print("PEFT model loaded")
+
+    def __call__(self, prompt: str) -> str:
+        input_ids = self._tokenizer(prompt, return_tensors='pt').input_ids.to(self.device)
+        out_ids = self._model.generate(input_ids=input_ids,
+                                    max_length=250,
+                                    eos_token_id=self._tokenizer.eos_token_id,
+                                    early_stopping=True,
+                                    do_sample=True,
+                                    temperature=0.8,
+                                    top_k=50,
+                                    top_p=0.85)
+        output = self._tokenizer.decode(out_ids[0][1:]).replace('<extra_id_0>','')
+        if '</s>' in output:
+            output = output[:output.find('</s>')].strip()
+        return output
---
+++ src/UserDialog.py
@@ -0,0 +1,14 @@
+from .FREDModel import FREDModel
+import torch
+
+
+class UserDialog:
+    def __init__(self, first_prompt: str, model_path: str, device: torch.device) -> None:
+        self._dialog = ['<SC1>' + first_prompt + 'Ваш первый диалог:']
+        self._model = FREDModel(model_path, device)
+
+    def reply_to_message(self, message: str) -> str:
+        self._dialog.append(message)
+        prompt = '\n'.join(self._dialog) + ' Твой ответ:<extra_id_0>'
+        reply = self._model(prompt)
+        return reply
