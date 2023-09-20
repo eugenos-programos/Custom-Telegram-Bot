c8d47a4cbf041e2dfda95fde8a2707ed75d4220d
Yauheni Samchuk <71227355+eugenos-programos@users.noreply.github.com>
2023-09-20 17:01:29 +0300

    fix UserDialog.py

--- src/UserDialog.py
+++ src/UserDialog.py
@@ -5,7 +5,7 @@ import torch
 class UserDialog:
     def __init__(self, first_prompt: str, model_path: str, device: torch.device) -> None:
         self._dialog = ['<SC1>' + first_prompt + 'Ваш первый диалог:']
-        self._first_prompt
+        self._first_prompt = first_prompt
         self._model = FREDModel(model_path, device)
 
     def reply_to_message(self, message: str) -> str:
