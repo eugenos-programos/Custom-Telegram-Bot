12fe1ce8b5fd489582342afb30e428426027657c
eugenos-programos <elena.samchuk.73@mail.ru>
2023-09-26 12:35:46 +0300

    refactor: remove unused imports

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from src import create_bot, UserDialog
 import os
-from dotenv import load_dotenv
 from settings import configs
 import torch
 
 
-load_dotenv("settings/key.env")
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 bot = create_bot(BOT_TOKEN, UserDialog(**configs, device=torch.device("cuda" if torch.cuda.is_available() else "cpu")))
--- src/FREDModel.py
+++ src/FREDModel.py
@@ -1,4 +1,3 @@
-from typing import Any
 from peft import PeftModel
 import torch
 import transformers
